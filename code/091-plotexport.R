8015bea86d669da4877b32daf9845e0b7722003f
Chris Mantegna <christine.mantegna@gmail.com>
2024-10-08 21:03:04 -0700

    plotting

--- code/091-plotexport.R
+++ code/091-plotexport.R
@@ -1,19 +1,21 @@
 
-# Load data
-data<- read.csv("/Users/cmantegna/Documents/WDFWmussels/data/p450data.csv")
-indv<- read.csv("/Users/cmantegna/Documents/WDFWmussels/data/avgP450_analytes.csv")
+# using this to export correlation plots since I can't seem to export/ save them properly in markdown
 
+# data
+data<- read.csv("/Users/cmantegna/Documents/WDFWmussels/data/p450correlationMAN.csv")
+
+# p450 plot
 library(corrplot)
 
 # Spearman correlation tests to extract p-values
-corr_results <- sapply(indv[ , 3:ncol(indv)], function(x) {
-  test <- cor.test(indv$avg_value, x, method = "spearman")
+corr_results <- sapply(data[ , 3:ncol(data)], function(x) {
+  test <- cor.test(data$avg_p450, x, method = "spearman")
   test$p.value  # Extract p-values
 })
 
 # Spearman correlation matrix for plotting
-spearman_cor_matrix <- cor(indv[ , 3:ncol(indv)], use = "complete.obs", method = "spearman")
-spearman_cor_avg_value <- spearman_cor_matrix["avg_value", ]
+spearman_cor_matrix <- cor(data[ , 3:ncol(data)], use = "complete.obs", method = "spearman")
+spearman_cor_avg_value <- spearman_cor_matrix["avg_p450", ]
 
 # Filter for significance using corr_results for p-values
 significant_analytes <- names(corr_results[corr_results < 0.05])
