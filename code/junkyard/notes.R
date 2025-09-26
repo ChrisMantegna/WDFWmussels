628a3145fb35ceda2ee6a4c42da240b093e54cec
Chris Mantegna <christine.mantegna@gmail.com>
2025-09-26 15:12:17 -0700

    analysis review

--- code/junkyard/notes.R
+++
@@ -1,7 +0,0 @@
-
-# keep NA values but exclude them from analyses
-mean(df$metric1, na.rm = TRUE)  # Computes mean without NA values
-sum(df$metric2, na.rm = TRUE)    # Sums values while ignoring NA
-
-# add a ggsave - I always forget it
-# add anything I use regularly but forget
